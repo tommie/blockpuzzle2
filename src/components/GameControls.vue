b9b8843bd73ccf9567b1d3c5897f8fbad875fd47
Tommie Gannert <tommie@gannert.se>
2025-09-30 10:02:54 +0200

    Fixes buttons for touch screen.
    Focus on touch screens makes Bootstrap toggle buttons behave weirdly
    as even outlined buttons are filled solid.

--- src/components/GameControls.vue
+++ src/components/GameControls.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { useGameStore } from '@/stores/game'
 import { useSettingsStore } from '@/stores/settings'
+import { useId } from 'vue'
 
 const gameStore = useGameStore()
 const settingsStore = useSettingsStore()
+
+const animId = useId()
+const soundId = useId()
 
 function handleNewGame() {
   gameStore.newGame()
@@ -22,20 +26,27 @@ function toggleSound() {
   <div class="d-flex justify-content-center gap-3">
     <button @click="handleNewGame" class="btn btn-primary" :disabled="false">New Game</button>
 
-    <button
-      class="btn"
-      :class="[settingsStore.animationsEnabled ? 'btn-light' : 'btn-outline-light']"
-      @click="toggleAnimations"
-    >
-      Animations
-    </button>
+    <input
+      type="checkbox"
+      class="btn-check"
+      :id="animId"
+      :value="settingsStore.animationsEnabled"
+      @change="toggleAnimations"
+    />
+    <label class="btn btn-secondary" :for="animId">
+      <i class="bi bi-film" />
+    </label>
 
-    <button
-      class="btn"
-      :class="[settingsStore.soundEnabled ? 'btn-light' : 'btn-outline-light']"
-      @click="toggleSound"
-    >
-      Sound
-    </button>
+    <input
+      type="checkbox"
+      class="btn-check"
+      :id="soundId"
+      :value="settingsStore.soundEnabled"
+      @change="toggleSound"
+    />
+    <label class="btn btn-secondary" :for="soundId">
+      <i v-if="settingsStore.soundEnabled" class="bi bi-volume-up-fill" />
+      <i v-else class="bi bi-volume-mute-fill" />
+    </label>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
